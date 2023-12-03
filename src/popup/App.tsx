e705117d728a4f60cea16d3d44461b01f7059572
Pramendra Gupta <pramendra@gmail.com>
2023-12-03 17:17:18 +0900

    updated popup yi

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,24 +1,30 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import "./../global.css";
+import { useState } from 'react';
+import './../global.css';
+import './index.css';
 
-function App() {
+function PopupApp() {
   const [count, setCount] = useState(0);
-  const appName = chrome.i18n.getMessage("appName");
-
+  const appName = chrome.i18n.getMessage('appName');
+  const handleButtonClick = () => {
+    chrome.runtime.sendMessage({ action: 'startCaption' });
+  };
   return (
     <>
-      <div className="card bg-red-50">
+      <div className="popup-container bg-red-50 flex flex-col">
         <h1>{appName}</h1>
+        <button className="btn btn-primary" onClick={handleButtonClick}>
+          start live caption
+        </button>
+        <p className="py-4"></p>
         <button
           className="btn btn-primary"
           onClick={() => setCount((count) => count + 1)}
         >
-          {chrome.i18n.getMessage("count", count.toString())}
+          {chrome.i18n.getMessage('count', count.toString())}
         </button>
       </div>
     </>
   );
 }
 
-export default App;
+export default PopupApp;
